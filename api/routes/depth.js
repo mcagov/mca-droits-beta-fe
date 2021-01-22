fd56f453182851d4671cfc34ccd36593dd32becb
ste-holmes <s.holmes@ctidigital.com>
2021-01-22 11:31:54 +0000

    Add route to check previous answers before displaying item-depth page - MCA-56

--- api/routes/depth.js
+++ api/routes/depth.js
@@ -3,8 +3,25 @@ const { body, validationResult } = require('express-validator');
 import { formatValidationErrors } from '../../utils';
 
 export default function (app) {
+  app.all('/report/depth', function (req, res) {
+    // Get the answer from session data
+    // The name between the quotes is the same as the 'name' attribute on the input elements
+    // However in JavaScript we can't use hyphens in variable names
+  
+    const salvagedFrom = req.session.data['removed-from']
+  
+    var depthResponses = ['shipwreck', 'seabed']
+  
+    // If it's not one of the depth responses, skip this question.
+    if (depthResponses.includes(salvagedFrom)) {
+      res.render('report/depth')
+    } else {
+      res.redirect('/report/vessel-description')
+    }
+  })
+
   app.post(
-    '/report/depth',
+    '/report/depth-answer',
     [
       body('vessel-depth')
         .exists()
@@ -14,15 +31,15 @@ export default function (app) {
     ],
     function (req, res) {
       const errors = formatValidationErrors(validationResult(req));
-      if (errors) {
+      if (!errors) {
+        res.redirect('report/vessel-description');
+      } else {
         return res.render('report/depth', {
           errors,
           errorSummary: Object.values(errors),
           values: req.body
         });
       }
-
-      res.redirect('report/vessel-description');
     }
   );
 }

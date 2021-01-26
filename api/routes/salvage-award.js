634dd0501f008657605553618944ed1f0c7cb99b
ste-holmes <s.holmes@ctidigital.com>
2021-01-26 14:45:16 +0000

    Add salvage-award page with routing, and validation. Add placeholder content to property-summary page

---
+++ api/routes/salvage-award.js
@@ -0,0 +1,38 @@
+const { body, validationResult } = require('express-validator');
+
+import { formatValidationErrors } from '../../utils';
+
+export default function (app) {
+  app.get('report/salvage-award', function (req, res) {
+    return res.render('report/salvage-award');
+  })
+  
+
+  app.post(
+    '/report/salvage-award-answer',
+    [
+      body('claim-salvage')
+        .exists()
+        .notEmpty()
+        .withMessage('Select yes if you wish to claim a salvage award')
+    ],
+    function (req, res) {
+      const errors = formatValidationErrors(validationResult(req));
+
+      if (!errors) {
+        req.session.data['claim-salvage'] = req.body['claim-salvage'];
+        if (req.body['salvage-services']) {
+          req.session.data['salvage-services'] = req.body['salvage-services'];
+        }
+        res.redirect('check-your-answers');
+
+      } else {
+        return res.render('report/salvage-award', {
+          errors,
+          errorSummary: Object.values(errors),
+          values: req.body
+        });
+      }
+    }
+  );
+}

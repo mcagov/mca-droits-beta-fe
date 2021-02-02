e106d8f1b5225bcc490ceeda25d0f2b23f794eb8
ste-holmes <s.holmes@ctidigital.com>
2021-02-02 15:00:12 +0000

    Add validation, and resolve issues with data persistence on address page

--- api/routes/property-form-address.js
+++ api/routes/property-form-address.js
@@ -4,7 +4,7 @@ import { formatValidationErrors } from '../../utils';
 
 export default function (app) {
 
-  app.all('/report/property-form-address/:prop_id', function (req, res) {
+  app.get('/report/property-form-address/:prop_id', function (req, res) {
     var rawPropertyID = req.params.prop_id;
     var propertyID;
     var propertyItem;
@@ -20,17 +20,61 @@ export default function (app) {
     res.render('report/property-form-address', { propertyID: propertyID, propertyItem: propertyItem });
   })
 
-  app.post('/report/property-form-address-answer/:prop_id', function (req, res) {
+  app.post(
+    '/report/property-form-address-answer/:prop_id', 
+    [
+      body('address-line-1')
+        .exists()
+        .not()
+        .isEmpty()
+        .withMessage('Enter your building and street'),
+      body('address-town')
+        .exists()
+        .not()
+        .isEmpty()
+        .withMessage('Enter your town or city'),
+      body('address-county')
+        .exists()
+        .not()
+        .isEmpty()
+        .withMessage('Enter your county'),
+      body('address-postcode')
+        .exists()
+        .not()
+        .isEmpty()
+        .withMessage('Enter your postcode')
+    ],
+    function (req, res) {
     var rawPropertyID = req.params.prop_id;
-    var sessionProperty = req.session.data.property[rawPropertyID];
+    var propertyID;
+    var propertyItem;
+
+    var property = req.session.data.property;
+    //var sessionProperty = req.session.data.property[rawPropertyID];
     var bodyProperty = req.body.property[rawPropertyID];
+
+    property[rawPropertyID]['storage-address'] = req.body.property[rawPropertyID]['storage-address']; 
+
+    if (property[rawPropertyID] !== undefined) {
+      propertyID = rawPropertyID;
+      propertyItem = property[propertyID];
+    }
+
+    const errors = formatValidationErrors(validationResult(req));
 
     if (bodyProperty['storage-address'] === 'custom') {
       for (let key in bodyProperty) {
-        sessionProperty[key] = bodyProperty[key];
+        console.log(bodyProperty);
+        property[propertyID][key] = bodyProperty[key];
+
+        return res.render('report/property-form-address', {
+          propertyID: propertyID,
+          propertyItem: propertyItem,
+          errors,
+          errorSummary: Object.values(errors),
+          values: req.body
+        });
       }
-    } else {
-      sessionProperty['storage-address'] = bodyProperty['storage-address'];
     }
 
     res.redirect('/report/property-summary');

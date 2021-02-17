656c5c809b86a432480462170658677c0269d65f
ste-holmes <s.holmes@ctidigital.com>
2021-02-17 11:18:18 +0000

    MCA-66 - Start work on implementing validation and error messages

--- app/js/components/bulkUpload.js
+++ app/js/components/bulkUpload.js
@@ -16,6 +16,8 @@ export class BulkUpload {
     this.containersUploaded = [...$('.photo-upload__container--uploaded', this.el)];
     this.photoResults = [...$('.photo-upload__result', this.el)];
     this.replaceImageButtons = [...$('.photo-upload__button-change', this.el)];
+    this.errorBlock = $('.upload-error', this.el);
+    this.errorText = $('.upload-error__text', this.el);
     this.uploadButton = $1('.photo-upload__button', this.el);
     this.addButton = $1('[data-js=bulk-add-btn]', this.el);
 
@@ -52,17 +54,24 @@ export class BulkUpload {
         }
       )
       .then((res) => {
-        const data = res.data;
-        this.photoResults.forEach((item, index) => {
-          console.log(item.filename);
-          item.src = `/uploads/${data[index]['filename']}`;
-          this.containersUploaded[index].classList.remove(
-            'photo-upload__container--hide'
-          );
-        })
-        console.log(res.data);
-        // Allow user to continue through form when images have uploaded
-        this.removeDisabledState();
+        if (res.data.error) {
+          this.errorText.forEach((i) => (i.innerText = res.data.error.text));
+          this.scrollToTop();
+          this.errorBlock.forEach((i) => (i.style.display = 'block'));
+        } else {
+          const data = res.data;
+          this.photoResults.forEach((item, index) => {
+            console.log(item.filename);
+            item.src = `/uploads/${data[index]['filename']}`;
+            this.containersUploaded[index].classList.remove(
+              'photo-upload__container--hide'
+            );
+          })
+
+          console.log(res.data);
+          // Allow user to continue through form when images have uploaded
+          this.removeDisabledState();
+        }
       })
       .catch((error) => {
         console.error(error);

312119c85e0af4634a5b0783002624a88c2cf330
Adam Gilmour <adam.gilmour@ctidigital.com>
2021-02-26 15:20:38 +0000

    Property form validation fix * Item quantity - add validation to check for a number * Item value - add validation to check for a number

--- api/routes/property-form.js
+++ api/routes/property-form.js
@@ -3,56 +3,59 @@ const { body, validationResult } = require('express-validator');
 import { formatValidationErrors } from '../../utils';
 
 export default function (app) {
-  
-  app.get('/report/property-form/:prop_id',
-    function (req, res) {
-      var propertyID;
-      // Instantiate a counter for the ID in the session data to avoid duplicates.
-      req.session.data['property-id-counter'] = req.session.data['property-id-counter'] !== undefined ? req.session.data['property-id-counter'] : 0;
+  app.get('/report/property-form/:prop_id', function (req, res) {
+    var propertyID;
+    // Instantiate a counter for the ID in the session data to avoid duplicates.
+    req.session.data['property-id-counter'] =
+      req.session.data['property-id-counter'] !== undefined
+        ? req.session.data['property-id-counter']
+        : 0;
 
-      var rawPropertyID = req.params.prop_id;
-      var property = req.session.data.property;
+    var rawPropertyID = req.params.prop_id;
+    var property = req.session.data.property;
 
-      // If our ID is the string "new", we're creating a new item of property.
-      if (rawPropertyID === 'new') {
-        // Generate the new ID and increment the counter for next time.
-        // Explaination of why we append an "i": https://stackoverflow.com/a/22198251
-        propertyID = 'i' + req.session.data['property-id-counter'];
-        req.session.data['property-id-counter']++;
-      } else {
-        // Otherwise we check if we're getting an existing ID and throw a 404 otherwise.
-        if (property[rawPropertyID] !== undefined) {
-          propertyID = rawPropertyID;
-        } else {
-          res.redirect('/report/property-summary');
-        }
-      }
-
-      res.render('report/property-form', { propertyID: propertyID });
-    }
-  )
-
-  app.post(
-    '/report/property-form/:prop_id',
-    async (req, res, next) => {      
-      var rawPropertyID = req.params.prop_id;
-
-      if(!req.session.data.property[rawPropertyID]) {
-        req.session.data.property[rawPropertyID] = req.body.property[rawPropertyID];
-      }
-
-      req.session.data.property[rawPropertyID].description = req.body.property[rawPropertyID].description;
-      req.session.data.property[rawPropertyID]['quantity'] = req.body.property[rawPropertyID].quantity;
-      req.session.data.property[rawPropertyID]['value-known'] = req.body['value-known'];
-      req.session.data.property[rawPropertyID]['value'] = req.body.property[rawPropertyID].value;
-
-      var property = req.session.data.property;
-      var propertyID;
-      var propertyItem;
-
+    // If our ID is the string "new", we're creating a new item of property.
+    if (rawPropertyID === 'new') {
+      // Generate the new ID and increment the counter for next time.
+      // Explaination of why we append an "i": https://stackoverflow.com/a/22198251
+      propertyID = 'i' + req.session.data['property-id-counter'];
+      req.session.data['property-id-counter']++;
+    } else {
+      // Otherwise we check if we're getting an existing ID and throw a 404 otherwise.
       if (property[rawPropertyID] !== undefined) {
         propertyID = rawPropertyID;
-        propertyItem = property[propertyID];
+      } else {
+        res.redirect('/report/property-summary');
+      }
+    }
+
+    res.render('report/property-form', { propertyID: propertyID });
+  });
+
+  app.post('/report/property-form/:prop_id', async (req, res, next) => {
+    var rawPropertyID = req.params.prop_id;
+
+    if (!req.session.data.property[rawPropertyID]) {
+      req.session.data.property[rawPropertyID] =
+        req.body.property[rawPropertyID];
+    }
+
+    req.session.data.property[rawPropertyID].description =
+      req.body.property[rawPropertyID].description;
+    req.session.data.property[rawPropertyID]['quantity'] =
+      req.body.property[rawPropertyID].quantity;
+    req.session.data.property[rawPropertyID]['value-known'] =
+      req.body['value-known'];
+    req.session.data.property[rawPropertyID]['value'] =
+      req.body.property[rawPropertyID].value;
+
+    var property = req.session.data.property;
+    var propertyID;
+    var propertyItem;
+
+    if (property[rawPropertyID] !== undefined) {
+      propertyID = rawPropertyID;
+      propertyItem = property[propertyID];
 
       await body('property' + '[' + propertyID + '][description]')
         .exists()
@@ -62,58 +65,76 @@ export default function (app) {
         .run(req);
       await body('property' + '[' + propertyID + '][quantity]')
         .exists()
+        .isNumeric()
+        .withMessage('Enter a number')
         .not()
         .isEmpty()
-        .withMessage('Enter how many items have been found that match this description')
+        .withMessage(
+          'Enter how many items have been found that match this description'
+        )
         .run(req);
       await body('value-known')
         .exists()
         .not()
         .isEmpty()
-        .withMessage('Select yes if you know the approximate value of the items that match this description')
+        .withMessage(
+          'Select yes if you know the approximate value of the items that match this description'
+        )
         .run(req);
-      } 
-      const errors = formatValidationErrors(validationResult(req));
-      
-      if (!errors) {
-        res.render('report/property-form-image', { propertyID: propertyID, propertyItem: propertyItem });
-      }
-      else {
-        return res.render('report/property-form', {
-          propertyID: propertyID, 
-          errors,
-          errorSummary: Object.values(errors),
-          values: req.body
-        });
+
+      if (req.body['value-known'] === 'yes') {
+        await body('property' + '[' + propertyID + '][value]')
+          .exists()
+          .isNumeric()
+          .withMessage('Enter a number')
+          .not()
+          .isEmpty()
+          .withMessage('Enter approximate value')
+          .run(req);
       }
     }
-  )
-  
-  app.get('/report/property-delete/:prop_id', function (req, res) {
-  
-    var rawPropertyID = req.params.prop_id
-    var property = req.session.data.property
-    var propertyItem = null
-  
-    if (property[rawPropertyID] !== undefined) {
-      propertyItem = property[rawPropertyID]
+    const errors = formatValidationErrors(validationResult(req));
+
+    if (!errors) {
+      res.render('report/property-form-image', {
+        propertyID: propertyID,
+        propertyItem: propertyItem
+      });
     } else {
-      res.redirect('/report/property-summary')
+      return res.render('report/property-form', {
+        propertyID: propertyID,
+        errors,
+        errorSummary: Object.values(errors),
+        values: req.body
+      });
     }
-  
-    res.render('report/property-delete', { propertyID: rawPropertyID, propertyItem: propertyItem })
-  })
-  
-  app.get('/report/property-delete-action/:prop_id', function (req, res) {
-  
-    var rawPropertyID = req.params.prop_id
-    var property = req.session.data.property
-  
+  });
+
+  app.get('/report/property-delete/:prop_id', function (req, res) {
+    var rawPropertyID = req.params.prop_id;
+    var property = req.session.data.property;
+    var propertyItem = null;
+
     if (property[rawPropertyID] !== undefined) {
-      delete property[rawPropertyID]
+      propertyItem = property[rawPropertyID];
+    } else {
+      res.redirect('/report/property-summary');
     }
-  
-    res.redirect('/report/property-summary')
-  })
-  
+
+    res.render('report/property-delete', {
+      propertyID: rawPropertyID,
+      propertyItem: propertyItem
+    });
+  });
+
+  app.get('/report/property-delete-action/:prop_id', function (req, res) {
+    var rawPropertyID = req.params.prop_id;
+    var property = req.session.data.property;
+
+    if (property[rawPropertyID] !== undefined) {
+      delete property[rawPropertyID];
+    }
+
+    res.redirect('/report/property-summary');
+  });
 }

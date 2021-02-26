09f23f1f0b0a34f0aee758ef19fee1f4f25d204c
Adam Gilmour <adam.gilmour@ctidigital.com>
2021-02-26 15:58:50 +0000

    * Ensured data gets added to session for depth, vessel name/construction/year, salvage services * Rounded down location radius number * Vessel depth now an int

--- api/routes/location.js
+++ api/routes/location.js
@@ -412,7 +412,7 @@ export default function (app) {
       session['location-standard'].longitude = parseFloat(
         session['location-standard'].longitude
       );
-      session['location-standard'].radius = parseFloat(
+      session['location-standard'].radius = parseInt(
         session['location-standard'].radius
       );
 
--- api/routes/vessel-information.js
+++ api/routes/vessel-information.js
@@ -1,27 +1,14 @@
-const { body, validationResult } = require('express-validator');
-
-import { formatValidationErrors } from '../../utils';
-
 export default function (app) {
-  app.post(
-    '/report/vessel-information-answer',
-    function (req, res) {
+  app.post('/report/vessel-information-answer', function (req, res) {
+    req.session.data['vessel-information']['vessel-name'] =
+      req.body['vessel-name'];
 
-      req.session.data['vessel-information'] = {};
+    req.session.data['vessel-information']['vessel-construction-year'] =
+      req.body['vessel-construction-year'];
 
-      if(req.body['vessel-name']) {
-        req.session.data['vessel-information']['vessel-name'] = req.body['vessel-name']
-      }
-      
-      if(req.body['vessel-construction-year']) {
-        req.session.data['vessel-information']['vessel-construction-year'] = req.body['vessel-construction-year'];
-      }
+    req.session.data['vessel-information']['vessel-sunk-year'] =
+      req.body['vessel-sunk-year'];
 
-      if(req.body['vessel-sunk-year']) {
-        req.session.data['vessel-information']['vessel-sunk-year'] = req.body['vessel-sunk-year']
-      }
-      
-      res.redirect('salvaged-from');
-    }
-  );
+    res.redirect('salvaged-from');
+  });
 }

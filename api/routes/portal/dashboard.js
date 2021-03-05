d50ad69f0e5b95879918672b1b20406bc92d8bf0
ste-holmes <s.holmes@ctidigital.com>
2021-03-05 11:59:08 +0000

    MCA-177 - Set up endpoint to retrieve access token, and initial api call to the db

---
+++ api/routes/portal/dashboard.js
@@ -0,0 +1,31 @@
+import adal from 'adal-node';
+
+export default function (app) {
+  app.post('/portal/access-dataverse', function (req, res) {
+
+    const adalAuthContext = adal.AuthenticationContext;
+
+    const authorityHostUrl = 'https://login.microsoftonline.com';
+    const tenant = '513fb495-9a90-425b-a49a-bc6ebe2a429e';
+    const authorityUrl = authorityHostUrl + '/' + tenant;
+    const clientId = '62ef4f36-0bd1-43f0-9ce4-eaf4a078b9a1';
+    const clientSecret = 'H9Z5g5N0VN~AV2.g~n1UP_8Wn9l.-0u2N_';
+    const resource = 'https://mca-sandbox.crm11.dynamics.com';
+
+    const context = new adalAuthContext(authorityUrl);
+
+    context.acquireTokenWithClientCredentials(
+      resource,
+      clientId,
+      clientSecret,
+      (err, tokenResponse) => {
+        if (err) {
+          console.log(`Token generation failed due to ${err}`);
+        } else {
+          console.log('else');
+          res.json(tokenResponse.accessToken);
+        }
+      }
+    );
+  });
+}

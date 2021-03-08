2531ad5bcaa50e83b94275a8998f1ac00bcd4780
ste-holmes <s.holmes@ctidigital.com>
2021-03-08 20:25:08 +0000

    MCA-177 - target specific db data and format api response

--- api/routes/portal/dashboard.js
+++ api/routes/portal/dashboard.js
@@ -1,20 +1,30 @@
 import adal from 'adal-node';
 import axios from 'axios';
+import dayjs from 'dayjs';
 
 export default function (app) {
   app.post('/portal/dashboard', function (req, res) {
-    console.log('test');
     const adalAuthContext = adal.AuthenticationContext;
-
     const authorityHostUrl = 'https://login.microsoftonline.com';
     const tenant = '513fb495-9a90-425b-a49a-bc6ebe2a429e';
     const authorityUrl = authorityHostUrl + '/' + tenant;
     const clientId = '62ef4f36-0bd1-43f0-9ce4-eaf4a078b9a1';
     const clientSecret = 'H9Z5g5N0VN~AV2.g~n1UP_8Wn9l.-0u2N_';
     const resource = 'https://mca-sandbox.crm11.dynamics.com';
-    const url = 'https://mca-sandbox.crm11.dynamics.com/api/data/v9.1/crf99_mcawreckreports';
+    const url = 'https://mca-sandbox.crm11.dynamics.com/api/data/v9.1/crf99_mcawreckreports?$select=crf99_reportreference,crf99_datereported,crf99_datefound,modifiedon,_crf99_reporter_value';
     // Contains 1 test report, with a second added from the api response:
     let userReports = [
+      {
+        dates: {
+          "date-found": "28 12 2019",
+          "date-reported": "15 01 2020",
+          "last-updated": "05 03 2021"
+        },
+        "report-ref": "99/21"
+      }
+    ];
+    
+    /* Example of a full dataset for reference
       { '@odata.etag': 'W/"1379431"',
         crf99_longitude: -3,
         statecode: 0,
@@ -67,7 +77,7 @@ export default function (app) {
         crf99_what3words: null,
         _owningteam_value: null,
         crf99_legacyfilereference: null }
-    ];
+    */
 
     const context = new adalAuthContext(authorityUrl);
 
@@ -79,38 +89,66 @@ export default function (app) {
         if (err) {
           console.log(`Token generation failed due to ${err}`);
         } else {
-          fetchData(tokenResponse.accessToken);
+          const accessToken = tokenResponse.accessToken;
+          fetchData(accessToken).then(() => {
+            return res.render('portal/dashboard', {userReports: userReports});
+          });
         }
       }
     );
 
-    function fetchData(accessToken) {      
-      axios.get(
-        url,
-        {
-          headers: { 'Authorization': `bearer ${accessToken}` },
-        }
-      )
-      .then((res) => {        
-        // For testing purposes we are hard coding a userID here, but the aim is to use the 
-        // inputted email address to query 'contacts' and grab the userID, before querying 
-        // 'mcawreckreports' to retrieve the wreck data related to that userID.
-        // The userID is the _crf99_reporter_value
-        const currentUserID = "8da6141c-727b-eb11-a812-0022481a85d1";
-        const data = res.data.value;
-        data.find(function(item, index) {
-          if(item._crf99_reporter_value === currentUserID) {
-            userReports.push(item);
+    function fetchData(accessToken) {  
+      return new Promise((resolve, reject) => {  
+        axios.get(
+          url,
+          {
+            headers: { 'Authorization': `bearer ${accessToken}` },
           }
-        })   
-        console.log(userReports);   
-        console.log('END');
-      }) 
-      .catch((reqError) => {
-        console.error(reqError);
+        )
+        .then((res) => {        
+          // For testing purposes we are hard coding a userID here, but the aim is to use the 
+          // inputted email address to query 'contacts' and grab the userID, before querying 
+          // 'mcawreckreports' to retrieve the wreck data related to that userID.
+          // The obj key for user ids is _crf99_reporter_value
+
+          // Assume we have retrieved this from the 'contacts' table in the database
+          const currentUserID = "8da6141c-727b-eb11-a812-0022481a85d1";
+          const data = res.data.value;
+
+          data.find(function(item, index) {
+            if(item._crf99_reporter_value === currentUserID) {
+              formatReportData(item);
+            }
+          }) 
+          resolve()
+        })
+        .catch((reqError) => {
+          return reqError;
+        })
       })
     }
 
-    return res.render('portal/dashboard', { reportData: userReports });
+    function formatReportData(data) {
+      let reportItem = {
+        dates: {
+          "date-found": "",
+          "date-reported": "",
+          "last-updated": ""
+        },
+        "report-ref": "", 
+      };
+      const dates = reportItem.dates;
+
+      dates["date-found"] = data.crf99_datefound;
+      dates["date-reported"] = data.crf99_datereported;
+      dates["last-updated"] = data.modifiedon;
+      reportItem["report-ref"] = data.crf99_reportreference;
+      
+      Object.keys(dates).forEach((key) => {
+        dates[key] = dayjs(dates[key]).format("DD MM YYYY");
+      })
+
+      userReports.push(reportItem);
+    }
   });
 }

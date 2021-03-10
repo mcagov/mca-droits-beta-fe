15c6b8807c181cb9cce021476455c05090c35998
ste-holmes <s.holmes@ctidigital.com>
2021-03-10 22:31:45 +0000

    MCA-177 - Update template and route function to render wreck material items

--- api/routes/portal/dashboard.js
+++ api/routes/portal/dashboard.js
@@ -11,11 +11,11 @@ export default function (app) {
     const clientId = '62ef4f36-0bd1-43f0-9ce4-eaf4a078b9a1';
     const clientSecret = 'H9Z5g5N0VN~AV2.g~n1UP_8Wn9l.-0u2N_';
     const resource = 'https://mca-sandbox.crm11.dynamics.com';
-    
+
     let accessToken = '';
     const currentUserEmail = req.body.email;
     let currentUserID;
-    
+
     const url = 'https://mca-sandbox.crm11.dynamics.com/api/data/v9.1/';
     const contactsUrl = url + `contacts?$select=contactid,emailaddress1&$filter=emailaddress1 eq '${currentUserEmail}'`;
 
@@ -26,12 +26,17 @@ export default function (app) {
         'date-found': '28 12 2019',
         'date-reported': '15 01 2020',
         'last-updated': '05 03 2021',
+        'wreck-materials': [
+          'The bell of the SS Mendi. Includes the identifying marking \'Mendi\'.',
+          '17th-century Dutch Navy cannon weighing 56kg and 1262mm in length',
+          'German U-boat propellers measuring 60cm in diameter.']
       },
       {
         'report-ref': '99/21',
         'date-found': '17 01 2020',
         'date-reported': '23 02 2020',
         'last-updated': '01 02 2021',
+        'wreck-materials': ['17th-century Dutch Navy cannon weighing 56kg and 1262mm in length']
       },
     ];
 
@@ -56,51 +61,52 @@ export default function (app) {
       }
     );
 
-    function getUserID(token) {  
-      return new Promise((resolve, reject) => {  
+    function getUserID(token) {
+      return new Promise((resolve, reject) => {
         axios.get(
           contactsUrl,
           {
             headers: { 'Authorization': `bearer ${token}` },
           }
         )
-        .then((res) => {        
-          const data = res.data.value;
-          currentUserID = data[0].contactid;
-          resolve()
-        })
-        .catch((reqError) => {
-          console.log('User ID error');
-          console.log(reqError);
-          reject();
-        })
+          .then((res) => {
+            const data = res.data.value;
+            currentUserID = data[0].contactid;
+            resolve()
+          })
+          .catch((reqError) => {
+            console.log('User ID error');
+            console.log(reqError);
+            reject();
+          })
       })
     }
 
     function fetchReportData(token, url) {
-      return new Promise((resolve, reject) => {  
+      return new Promise((resolve, reject) => {
         axios.get(
           url,
           {
             headers: { 'Authorization': `bearer ${token}` },
           }
         )
-        .then((res) => {        
-          const reportData = res.data.value;
-          reportData.forEach((item) => {       
-            formatReportData(item);
-          })
+          .then((res) => {
+            const reportData = res.data.value;
+            reportData.forEach((item) => {
+              formatReportData(item);
+            })
 
-          resolve();
-        })
-        .catch((reqError) => {
-          console.log('Report data error: ' + reqError);
-          reject();
-        })
+            resolve();
+          })
+          .catch((reqError) => {
+            console.log('Report data error: ' + reqError);
+            reject();
+          })
       })
     }
 
     function formatReportData(data) {
+      const wreckMaterialsData = data.crf99_MCAWreckMaterial_WreckReport_crf99_;
       let reportItem = {};
 
       reportItem['report-ref'] = data.crf99_reportreference;
@@ -113,7 +119,7 @@ export default function (app) {
       reportItem['last-updated'] = dayjs(data.modifiedon).format('DD MM YYYY');
       reportItem['wreck-materials'] = [];
 
-      ['wreck-materials'].forEach((item) => {
+      wreckMaterialsData.forEach((item) => {
         reportItem['wreck-materials'].push(item.crf99_description);
       });
 

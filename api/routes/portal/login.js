a62cac9a7d99b3eb9f057f80b6ff694a04d72113
ste-holmes <s.holmes@ctidigital.com>
2021-03-18 11:55:43 +0000

    Add env variables for portal login and dataverse api calls

--- api/routes/portal/login.js
+++ api/routes/portal/login.js
@@ -1,14 +1,16 @@
 import adal from 'adal-node';
+const dotenv = require('dotenv');
+dotenv.config();
 
 export default function (app) {
   app.post('/portal/login', function (req, res) {
     const adalAuthContext = adal.AuthenticationContext;
-    const authorityHostUrl = 'https://login.microsoftonline.com';
-    const tenant = '513fb495-9a90-425b-a49a-bc6ebe2a429e';
-    const authorityUrl = authorityHostUrl + '/' + tenant;
-    const clientId = '62ef4f36-0bd1-43f0-9ce4-eaf4a078b9a1';
-    const clientSecret = 'H9Z5g5N0VN~AV2.g~n1UP_8Wn9l.-0u2N_';
-    const resource = 'https://mca-sandbox.crm11.dynamics.com';
+    const authorityHostUrl = 'https://login.microsoftonline.com/';
+    const tenant = process.env.TENANT_ID;
+    const authorityUrl = authorityHostUrl + tenant;
+    const clientId = process.env.CLIENT_ID;
+    const clientSecret = process.env.CLIENT_SECRET;
+    const resource = process.env.DATAVERSE_API_BASE_URL;
     const context = new adalAuthContext(authorityUrl);
 
     const currentUserEmail = req.body.email;

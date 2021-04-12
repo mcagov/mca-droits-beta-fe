fd176f09f947355fd3aaf2cb35edd7623911458e
ste-holmes <s.holmes@ctidigital.com>
2021-04-12 13:43:21 +0100

    Update error logout redirects

--- api/routes/portal/ensureAuthenticated.js
+++ api/routes/portal/ensureAuthenticated.js
@@ -4,6 +4,6 @@ export default function ensureAuthenticated(req, res, next) {
   }
   req.session.destroy(function (err) {
     req.logOut();
-    res.redirect(`https://mcactitest.b2clogin.com/mcactitest.onmicrosoft.com/oauth2/v2.0/logout?p=B2C_1_signin&post_logout_redirect_uri=${process.env.ENV_BASE_URL}/error`);
+    res.redirect(`https://mcastaging.b2clogin.com/mcastaging.onmicrosoft.com/oauth2/v2.0/logout?p=B2C_1_login&post_logout_redirect_uri=${process.env.ENV_BASE_URL}/error`);
   });
 }

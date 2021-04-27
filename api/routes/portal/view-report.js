5ffb281ad7da49782f71bb74f1673fe2fae3ff77
ste-holmes <s.holmes@ctidigital.com>
2021-04-27 10:36:41 +0100

    Style report outcome content

--- api/routes/portal/view-report.js
+++ api/routes/portal/view-report.js
@@ -53,16 +53,13 @@ export default function (app) {
               reportData['recovered-from'] = recoveredFromLocation;
 
               const wreckMaterialData = reportData.crf99_MCAWreckMaterial_WreckReport_crf99_;
-              console.log(wreckMaterialData);
               for (const wreckItem of wreckMaterialData) {
                 // Add apostrophies before pushing to the storageAddressIDs array.
                 // This is required to pass the array straight into the storageAddressUrl to filter the storage address data.
                 const formattedValue = "'" + wreckItem._crf99_storageaddress_value + "'";
                 storageAddressIDs.push(formattedValue);
                 wreckItem.selectedOutcome = assignOutcome(wreckItem.crf99_outcome);
-                console.log(wreckItem.selectedOutcome);
               }
-              console.log(reportData);
               resolve();
             })
             .catch((err) => {

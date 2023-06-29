62ff3ed2eecc48cf235db4ca8494a81dbc55284a
Sam Kendell <sam.kendell@madetech.com>
2023-06-29 12:45:42 +0100

    Do not use session data at the moment

--- utilities/sessionData.js
+++ utilities/sessionData.js
@@ -1,24 +1,26 @@
 import sessionDataDefaults from '../api/data/session-data-defaults';
 
 export const sessionData = (req, res, next) => {
-  if (!req.session.data) {
-    req.session.data = {};
-  }
-  req.session.data = Object.assign(
-    {},
-    req.path.indexOf('report') > -1 && req.path.indexOf('portal') === -1
-      ? sessionDataDefaults
-      : null,
-    req.session.data
-  );
+  req.session.data = {};
 
-  // Send session data to all views
+  // if (!req.session.data) {
+  //   req.session.data = {};
+  // }
+  // req.session.data = Object.assign(
+  //   {},
+  //   req.path.indexOf('report') > -1 && req.path.indexOf('portal') === -1
+  //     ? sessionDataDefaults
+  //     : null,
+  //   req.session.data
+  // );
 
-  res.locals.data = {};
+  // // Send session data to all views
 
-  for (var j in req.session.data) {
-    res.locals.data[j] = req.session.data[j];
-  }
+  // res.locals.data = {};
+
+  // for (var j in req.session.data) {
+  //   res.locals.data[j] = req.session.data[j];
+  // }
 
   next();
 };
